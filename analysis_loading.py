1dc3226a424f0805dd5ec6b5baaadd02cf5695bd
Aidan McLaughlin <112657676+aidanmc0112@users.noreply.github.com>
2026-07-17 16:21:57 -0500

    Update analysis_loading.py

--- analysis_loading.py
+++ analysis_loading.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
-
-path = r"C:\Users\aidan\Downloads\lahmans_project\data\lahman_1871-2025_csv"
-
-batting = pd.read_csv(path + r"\Batting.csv")
-people = pd.read_csv(path + r"\People.csv")
-teams = pd.read_csv(path + r"\Teams.csv")
-awards = pd.read_csv(path + r"\AwardsSharePlayers.csv")
-fielding = pd.read_csv(path + r"\Fielding.csv")
+def load_data():
+  path = r"C:\Users\aidan\Downloads\lahmans_project\data\lahman_1871-2025_csv"
+  
+  batting = pd.read_csv(path + r"\Batting.csv")
+  people = pd.read_csv(path + r"\People.csv")
+  teams = pd.read_csv(path + r"\Teams.csv")
+  awards = pd.read_csv(path + r"\AwardsSharePlayers.csv")
+  fielding = pd.read_csv(path + r"\Fielding.csv")
+  return people, batting, teams, fielding, awards
